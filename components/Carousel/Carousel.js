39274ef5adc89adef3a36d998a6ffdd562f0db52
Areum Jo <areumjo1@gmail.com>
2019-06-21 12:56:36 -0400

    Stretch Problems - Created an infinite loop carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,57 @@
 class Carousel {
+    constructor(images) {
+        this.images = images;
+        console.log(this.images);
 
+        var imgArray = Array.from(this.images);
+        var currentImage = imgArray[0];
+        console.log(currentImage);
+        
+        var imageIndices = imgArray.map((img) => img.dataset.tab);
+        console.log(imageIndices);
+        
+        var currentImageIndex = currentImage.dataset.tab;
+
+        this.currentIndex = imageIndices.indexOf(currentImageIndex);
+        console.log(this.currentIndex);
+
+        this.images[this.currentIndex].style.display = "flex";
+
+        var leftBtn = document.querySelector('.left-button');
+        var rightBtn = document.querySelector('.right-button');
+
+        leftBtn.addEventListener('click', () => this.left());
+        rightBtn.addEventListener('click', () => this.right());
+
+    }
+    left() {
+        console.log('left work?');
+        this.images[this.currentIndex].style.display = "none";
+        //console.log(this.currentIndex);
+        this.currentIndex -= 1;
+        if (this.currentIndex == -1) {
+            this.currentIndex = 3;
+        }
+        this.images[this.currentIndex].style.display = "flex";
+    }
+    right() {
+        console.log("right work?")
+        this.images[this.currentIndex].style.display = "none";
+        //console.log(this.currentIndex);
+        this.currentIndex += 1;
+        if (this.currentIndex >= this.images.length) {
+            this.currentIndex = 0;
+        }
+        this.images[this.currentIndex].style.display = "flex";
+    }
 }
 
-let carousel = document.querySelector();
+const carousel = document.querySelector('.carousel');
+const images = document.querySelectorAll('.carousel img')
+// console.log(carousel);
+// console.log(images); //4 images
+
+toCarosel = new Carousel(images);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
